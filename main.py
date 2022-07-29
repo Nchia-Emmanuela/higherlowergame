969224609798e6128747c0c020dcd6c6d4e65cba
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-07-29 20:10:49 +0100

    function to collect details

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ from game_data import data
 def clear():
     os.system('cls')
 
+def format_data(account):
+    """Formating the account into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
+
 account_a = random.choice(data)
 account_b = random.choice(data)
